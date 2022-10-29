ef8ceff9b39346c2b5c124163ac8e961a6ca35f9
thetlwinlwin <minthantlwin8@gmail.com>
2022-10-29 23:19:35 +0630

    init

---
+++ app/extractor/scrape.py
@@ -0,0 +1,99 @@
+from typing import IO, DefaultDict
+from bs4 import BeautifulSoup as bs
+import re
+from collections import defaultdict
+import logging
+
+
+LIST_CLASS: str = "css-qmttav"
+PARA_CLASS: str = "cds-137 css-1j071wf cds-139"
+ALLOWED_TYPES: tuple = ("txt", "html")
+DEFAULT_SUFFIX: str = "EditDelete"
+DEFAULT_PREFIX: str = "Go to video"
+ADDED_NOTES_HEADER: str = "Your Notes"
+MY_REGEX: str = "\d:\d\d"
+
+
+def remove(input_text: str, to_remove=ADDED_NOTES_HEADER) -> str:
+    return "".join(re.split(to_remove, input_text))
+
+
+def note_from_source(input_text: str) -> list[str]:
+    result_list = re.split(MY_REGEX, input_text)
+
+    video_name = result_list[0]
+    unmodified_note = result_list.pop()
+
+    if ADDED_NOTES_HEADER in unmodified_note:
+        modified_note = remove(unmodified_note)
+        return [video_name, modified_note]
+
+    return [video_name, unmodified_note]
+
+
+def file_handler(filename, mode="r"):
+    try:
+        is_valid = any(filter(lambda x: filename.endswith(x), ALLOWED_TYPES))
+        if is_valid:
+            file = open(filename, mode)
+            return file
+        else:
+            raise Exception("Unsupported input data type.")
+    except OSError as e:
+        logging.warning(str(e))
+        raise Exception(
+            "Something is not right with your file. Make sure filename is correct and file location to be as same as downloader file."
+        )
+
+
+def merge_dict(result: list[str]) -> DefaultDict[str, list]:
+    d = defaultdict(list)
+    for k, v in result:
+        d[k].append(v)
+    return d
+
+
+def extract_notes(target_file: IO) -> list[str]:
+    result = []
+    soup = bs(target_file, "html.parser")
+    li = soup.find_all("li", {"class": LIST_CLASS})
+    for i in li:
+
+        stripped = i.text.lstrip(DEFAULT_PREFIX).rstrip(DEFAULT_SUFFIX)
+        x = note_from_source(stripped)
+
+        result.append(x)
+    if not result:
+        raise Exception("Unsupported input data type.")
+
+    return result
+
+
+def write_output(output_file: IO, notes: DefaultDict[str, list]):
+    for k, v in notes.items():
+        output_file.write("Video - " + k + "\n\n")
+        for index, val in enumerate(v, start=1):
+            output_file.write(str(index) + " - " + val + "\n")
+        output_file.write("\n-------\n")
+
+
+def input_processing(input: str) -> DefaultDict[str, list]:
+    notes = extract_notes(input)
+    return merge_dict(notes)
+
+
+def output_processing(
+    notes: DefaultDict[str, list],
+    output_name: str,
+) -> None:
+    output = file_handler(output_name, mode="w")
+    write_output(output, notes)
+    output.close()
+
+
+def get_notes(
+    ipnut_string: str,
+    output_name: str = "result.txt",
+):
+    notes: DefaultDict[str, list] = input_processing(input=ipnut_string)
+    output_processing(notes, output_name)
---
+++ app/main.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI, Form, Request, status, BackgroundTasks
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse, FileResponse
+from pathlib import Path,PurePath
+from typing import Optional
+from .extractor.scrape import get_notes
+
+
+def clean_up(filename):
+    file_path = Path.cwd().joinpath(filename)
+    if file_path.exists():
+        file_path.unlink()
+
+FOLDER_PATH  = Path.cwd().joinpath(__file__).parent
+STATIC_PATH = FOLDER_PATH.joinpath('static')
+TEMPLATE_PATH = FOLDER_PATH.joinpath('templates')
+print(f'{STATIC_PATH=}')
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory=STATIC_PATH), name="static")
+templates = Jinja2Templates(directory=TEMPLATE_PATH)
+
+
+@app.get("/", response_class=HTMLResponse)
+def index_html(request: Request):
+    return templates.TemplateResponse('index.html', {"request": request})
+
+
+@app.post("/get_form")
+def get_form(
+    request: Request,
+    note_source: str = Form(),
+    output_name:  Optional[str]= Form(None),
+):
+    if not output_name:
+        return error_page("please enter the output file name.", request)
+    if not output_name.endswith(".txt"):
+        output_name = output_name + ".txt"
+    try:
+
+        get_notes(note_source, output_name)
+
+    except Exception as e:
+        return error_page(str(e), request)
+    return RedirectResponse(
+        f"/result/{output_name}",
+        status_code=status.HTTP_303_SEE_OTHER,
+    )
+
+
+@app.get(
+    "/result/{file_name}",
+    response_class=HTMLResponse,
+)
+def get_result(
+    file_name: str,
+    request: Request,
+):
+    return templates.TemplateResponse(
+   'result.html',
+        {
+            "request": request,
+            "file_name": file_name,
+        },
+    )
+
+
+@app.get(
+    "/file/{file_name}",
+    response_class=FileResponse,
+)
+async def file(file_name: str, bg_tasks: BackgroundTasks):
+    bg_tasks.add_task(clean_up, file_name)
+    return FileResponse(
+        file_name,
+        media_type="text/plain",
+        background=bg_tasks,
+    )
+
+
+def error_page(error: str, request: Request):
+    return templates.TemplateResponse(
+    'error.html',
+        {
+            "request": request,
+            "error": error,
+        },
+    )
